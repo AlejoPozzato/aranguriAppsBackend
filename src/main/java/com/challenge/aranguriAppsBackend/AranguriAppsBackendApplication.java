5e557a900837ca598f68ac6c6b6ab9a1a19c66e5
AlejoPozzato <alejopozzato@gmail.com>
2025-09-01 13:59:00 -0300

    Inicialización y configuración del proyecto y arquitectura MVC

---
+++ src/main/java/com/challenge/aranguriAppsBackend/AranguriAppsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.aranguriAppsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AranguriAppsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AranguriAppsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/aranguriAppsBackend/AranguriAppsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.aranguriAppsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AranguriAppsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
